2b6465bcdcf3824d7b6f992ea0dbb036f32aee88
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-08 18:50:24 +0000

    Add clearcart and removeItem reducers

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from '../../cartItems'
 
 const initialState = {
   cartItems: cartItems,
-  amount: 0,
+  amount: 4,
   total: 0,
   isLoading: true,
 }
@@ -11,8 +11,30 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state, action) => {
+      state.cartItems = [];
+      // console.log(action)
+      // return { cartItems: [] };
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+      console.log(action)
+    },
+    increase: (state, { payload }) => {
+      // console.log(action);
+      console.log(payload);
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+  }
 })
 
 // console.log(cartSlice);
-
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer
