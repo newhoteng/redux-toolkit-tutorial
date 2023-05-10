ee2b28e6778f0ad101728519ad32afb35570c715
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-10 11:39:52 +0000

    Explore other ways to use asyncthunk using axios

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { openModal } from '../modal/modalSlice';
 // import cartItems from '../../cartItems';
 
 const url = 'https://course-api.com/react-useReducer-cart-project';
@@ -11,10 +13,27 @@ const initialState = {
   isLoading: true,
 };
 
-export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
-  return fetch(url)
-    .then((resp) => resp.json())
-    .catch((err) => console.log(err));
+// export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+//   return fetch(url)
+//     .then((resp) => resp.json())
+//     .catch((err) => console.log(err));
+// });
+
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  async (name, thunkAPI) => {
+    try {
+      // console.log(name);
+      // console.log(thunkAPI);
+      // console.log(thunkAPI.getState())
+      // thunkAPI.dispatch(openModal())
+      const resp = await axios(url);
+      // console.log(resp);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong')
+      // axios 'error.response'
+    }
 });
 
 const cartSlice = createSlice({
@@ -32,7 +51,7 @@ const cartSlice = createSlice({
       // console.log(action)
     },
     increase: (state, { payload }) => {
-      console.log(payload);
+      // console.log(payload);
       const cartItem = state.cartItems.find((item) => item.id === payload.id);
       cartItem.amount = cartItem.amount + 1;
     },
@@ -56,11 +75,12 @@ const cartSlice = createSlice({
       state.isLoading = true
     },
     [getCartItems.fulfilled]: (state, action) => {
-      console.log(action);
+      // console.log(action);
       state.isLoading = false;
       state.cartItems = action.payload;
     },
-    [getCartItems.rejected]: (state) => {
+    [getCartItems.rejected]: (state, action) => {
+      console.log(action);
       state.isLoading = false;
     },
   }
